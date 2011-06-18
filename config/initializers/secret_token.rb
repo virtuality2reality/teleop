ff18825ceb273554355678368055f166cde7199d
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2011-06-18 18:47:17 +0200

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Teleop::Application.config.secret_token = 'daa9f641f0ee9235c28b1ca7b2c4a73522a39e4f5b384bd3e8644e29a3fd8efe4b8b73d1014238d7dbdb5e0f25b6e6e85eb150117d4c60be65b47b31a43a1efb'
