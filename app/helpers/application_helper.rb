c2b7448f95401dd3e55931157929567ee6262cae
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2011-06-19 01:18:37 +0200

    Design template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,18 @@
 module ApplicationHelper
   
   def link_to_add_fields(name, f, association, options = {})
+    locals = options.delete(:locals) || {}
+    selector = options.delete(:selector)
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize + "_fields", options.merge(:f => builder))
+      render(association.to_s.singularize + "_fields", locals.merge(:f => builder))
     end
-    selector = options.delete(:selector)
-    link_to_function(name, "teleop.add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\", \"#{selector}\")")
+    link_to_function(name, "teleop.add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\", \"#{selector}\")", options)
   end
   
   def link_to_remove_fields(name, f, options = {})
-    selector = options[:selector] || "fieldset"
-    f.hidden_field(:_destroy) + link_to_function(name, "teleop.remove_fields(this, \"#{selector}\")")
+    selector = options.delete(:selector) || "fieldset"
+    f.hidden_field(:_destroy) + link_to_function(name, "teleop.remove_fields(this, \"#{selector}\")", options)
   end
   
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -14,3 +14,35 @@ var teleop = {
     $content.slideDown(200);
   }
 }
+
+$(document).ready(function (){
+
+/** 
+ * Sidebar menus
+ * Slidetoggle for menu list
+ * */
+var currentMenu = null; 
+$('#sidebar>ul>li[class!="nosubmenu"]>a').each(function(){
+    if(!$(this).parent().hasClass('current')){
+        $(this).parent().find('ul:first').hide();
+    }else{
+        currentMenu = $(this); 
+    }
+    $(this).click(function(){
+        $('#sidebar>ul>li.current').removeClass('current'); 
+        if(currentMenu != null && currentMenu.text() != $(this).text()){
+            currentMenu.parent().find('ul:first').slideUp(); 
+        }
+        if(currentMenu != null && currentMenu.text() == $(this).text()){
+            currentMenu.parent().find('ul:first').slideUp(); 
+            currentMenu = null;
+        }else{
+            currentMenu = $(this);
+            currentMenu.parent().addClass('current'); 
+            currentMenu.parent().find('ul:first').slideDown(); 
+        }
+        return false;
+    }); 
+});
+
+});
