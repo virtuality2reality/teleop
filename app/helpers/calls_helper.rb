28bcfb78f24685e94d876a42d3a7126edefe3826
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2011-07-13 19:41:58 +0200

    Fix hash order issue with Ruby 1.8.7

--- app/helpers/calls_helper.rb
+++ app/helpers/calls_helper.rb
@@ -1,14 +1,18 @@
 module CallsHelper
   def answers_for(f)
-    answers = {}
+    answers_by_section = []
     section = nil
     f.fields_for :answers do |builder|
       if builder.object.question.section != section
         section = builder.object.question.section
       end
-      answers[section.title] ||= []
-      answers[section.title] << render('answer_fields', :f => builder)
+      i = answers_by_section.index { |s| s[:section] == section.title }
+      if i.nil?
+        answers_by_section << { :section => section.title, :answers => [] }
+        i = answers_by_section.length - 1
+      end
+      answers_by_section[i][:answers] << render('answer_fields', :f => builder)
     end
-    answers
+    answers_by_section
   end
 end
